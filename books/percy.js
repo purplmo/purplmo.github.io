ccd1bf6789b77f259d8e730c190b932471d71d35
Monae <93678860+purplemo340@users.noreply.github.com>
2024-01-02 14:37:02 -0500

    new year new clock

--- books/percy.js
+++ books/percy.js
@@ -2,7 +2,7 @@ function displayTime(){
     let clock = document.querySelector("#clock");
     let now = new Date();
     let hour= 21-now.getHours() 
-    clock.textContent = 33-now.getDate() + " Days and " + hour + " Hours Until Next Episode"; //+ now.getHours();
+    clock.textContent = 2-now.getDate() + " Days and " + hour + " Hours Until Next Episode"; //+ now.getHours();
   }
   displayTime()
   setInterval(displayTime, 1000);
