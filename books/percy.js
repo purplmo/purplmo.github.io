9a7faf545660d4c921ccc972a36b24e0e5e460fe
Monae <93678860+purplemo340@users.noreply.github.com>
2024-01-18 22:28:41 -0500

    fix clock

--- books/percy.js
+++ books/percy.js
@@ -1,8 +1,13 @@
 function displayTime(){
     let clock = document.querySelector("#clock");
     let now = new Date();
-    let hour= 28-now.getHours() 
-    clock.textContent = 16-now.getDate() + " Days and " + hour + " Hours Until Next Episode"; //+ now.getHours();
+    let hour= 21-now.getHours() 
+    let day = 23
+    if(hour<0) {
+      day=day-1
+      hour=24+hour
+    }
+    clock.textContent = day-now.getDate() + " Days and " + hour + " Hours Until Next Episode"; //+ now.getHours();
   }
   displayTime()
   setInterval(displayTime, 1000);
